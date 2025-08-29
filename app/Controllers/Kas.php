75f2d592ba400da22ac11124313edf1dd6b8c424
dedyhariadi <dedy.hariadi@gmail.com>
2025-08-29 14:21:32 +0700

    update

--- app/Controllers/Kas.php
+++ app/Controllers/Kas.php
@@ -20,7 +20,7 @@ class Kas extends BaseController
     public function index()
     {
 
-        if ($this->request->getvar()) {
+        if ($this->request->getPost()) {
             $this->kasModel->insert([
                 'tanggal' => simpanTanggal($this->request->getPost('tanggal')),
                 'keterangan' => $this->request->getPost('uraian'),
